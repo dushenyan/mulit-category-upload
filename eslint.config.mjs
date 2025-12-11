34d5cf062e613812df0c16d4289cbb883f64fcde
Elegant <63347504+dushenyan@users.noreply.github.com>
2025-12-11 15:08:46 +0800

    feat: 添加ESLint配置及脚本命令

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+
+  type: 'lib',
+
+  jsonc: true,
+
+  typescript: true,
+
+}, {
+  files: ['**/*'],
+  rules: {
+    'no-console': 'off',
+    'ts/explicit-function-return-type': 'off',
+  },
+})
